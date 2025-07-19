f338785b037dbf2b716f93c1a16e98591e71a0a4
poonam835 <52094786+poonam835@users.noreply.github.com>
2025-07-19 09:58:58 +0000

    datatype

---
+++ 01_basics/dtypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+// alert (3+3) gives error because we are using node js not browswer, in browser it give output as 6 
+let number  = 3
+let string = "piu"
+console.log(typeof 3);// give output as number
+console.log(typeof "piu");// output as string
+let state;
+console.log(typeof state); // output as undefined which is correct
+let city = null
+console.log(typeof city); //output as object why? 
+
+
+
+
+//datatypes : Primitive 
+/* number = 3
+    string  = ""
+    bigint
+    boolean = true/false
+    null = standalone value // let state = null
+    undefined // let state;\
+    symbol - find unqiueness*/
--- 01_basics/var.js
+++ 01_basics/var.js
@@ -1,5 +1,23 @@
 const accountId = 1986 /*const can not be changed*/
 let accountEmail = "piu.97@gmail.com"
 var accountPassword = "987654321"
-accountCity = "gwalior"
-//aacountId = 1980 /* but here we can see itchanging but if you run it it show throw eror */ 
+accountCity = "gwalior" // also here without declaring a variable you can reserve a value, which is not a good practice
+//here we are checcking what we can change after declaring
+//aacountId = 1980  not allowed  
+accountEmail = "rohan@gmail.com"
+accountPassword = "11112222"
+let accountState // in js if you declare a variable without giving value it acts as undefined
+console.log(accountId);   
+// if we can change the value in let and var both then we use diff , 
+// we can use any of them ? so in js we can declare in both ways via let or var but prefer not to use var
+        //because of issue in block scope and fucntional scope
+console.log(accountEmail);
+console.log(accountPassword);
+//means var and let are re declarebale 
+
+//to make it easy we are using here console.table 
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]) // will print the vlaues in form of table
+
+
+
+
