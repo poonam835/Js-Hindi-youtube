1b96aa1f0b2ddf4eb1234a0c439813e8a0fbccb9
poonam835 <52094786+poonam835@users.noreply.github.com>
2025-07-19 08:55:23 +0000

    variable learning

---
+++ 01_basics/var.js
@@ -0,0 +1,5 @@
+const accountId = 1986 /*const can not be changed*/
+let accountEmail = "piu.97@gmail.com"
+var accountPassword = "987654321"
+accountCity = "gwalior"
+//aacountId = 1980 /* but here we can see itchanging but if you run it it show throw eror */ 
